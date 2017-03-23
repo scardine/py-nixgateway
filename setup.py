b923fe15c7486c1e9b0a9adfe9c9ae1fff8a175f
paulos <paulo.scardine@3mw.com.br>
2017-03-23 15:36:14 -0300

    Fix release links.

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup
 setup(
   name = 'nixgateway',
   packages = ['nixgateway'],
-  version = '0.1',
-  description = 'Python bindings to the Nix Gateway API ()',
+  version = '0.3',
+  description = 'Python bindings to the Nix Gateway API (https://web-nix.cloud.nexxera.com/)',
   author = 'Paulo Scardine',
   author_email = 'paulos@xtend.com.br',
   url = 'https://github.com/scardine/py-nixgateway',
-  download_url = 'https://github.com/scardine/py-nixgatway/archive/0.1.tar.gz',
+  download_url = 'https://github.com/scardine/py-nixgateway/archive/0.3.tar.gz',
   keywords = ['nexxera', 'nix gateway', 'payments', 'api'],
   classifiers = [],
+  install_requires=[
+    'python-jose',
+    'requests',
+  ],
 )
