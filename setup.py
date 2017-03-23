c7e18c9aba6f7d7b3c33ec631af69077cf2a7830
paulos <paulo.scardine@3mw.com.br>
2017-03-23 13:44:47 -0300

    Made it PyPI installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'nixgateway',
+  packages = ['nixgateway'],
+  version = '0.1',
+  description = 'Python bindings to the Nix Gateway API ()',
+  author = 'Paulo Scardine',
+  author_email = 'paulos@xtend.com.br',
+  url = 'https://github.com/scardine/py-nixgateway',
+  download_url = 'https://github.com/scardine/py-nixgatway/archive/0.1.tar.gz',
+  keywords = ['nexxera', 'nix gateway', 'payments', 'api'],
+  classifiers = [],
+)
